3e7c9c95983f9fc45d125e98141cbd6ef38e4692
Vipin Saini <starvipin36@gmail.com>
2025-12-17 20:33:26 +0530

    process videos to audios using ffmpag

---
+++ process_videos.py
@@ -0,0 +1,20 @@
+# convert the videos to mp3
+
+import os 
+import subprocess
+
+files = os.listdir("videos")
+# print(files)
+
+for file in files:
+    # print(file)
+    tutorial_number = file.split("_")[0].split(".")[0]
+    
+    file_name = file.split("_")[1]
+    print(tutorial_number,file_name)
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3" ])
+    
+
+# a = "12_dsd"
+
+# print(a.split("_")[0])
